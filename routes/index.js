679a3fdc52df20d5234f448b1e9a11b62b17ce3a
Andy Anderson <andyanderson1818@gmail.com>
2017-05-17 17:55:02 -0700

    Initial commit. Wes gave us a great start.

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const storeController = require('../controllers/storeController');
+
+router.get('/', storeController.homePage);
+
+
+
+
+
+
+
+
+
+module.exports = router;
+
+
+// Below is a fun method that Wes demo'ed. Reverses the string value in the query. This is never used.
+
+//router.get('/reverse/:name', (req,res) => {
+//const reverse = [...req.params.name].reverse().join('');
+//res.send(reverse);
+//});
